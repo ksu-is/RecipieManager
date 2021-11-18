d801bb8b416bf80414064af73bfa0ad6ccd91f18
bdilbeck <89172591+bdilbeck@users.noreply.github.com>
2021-11-18 18:38:25 -0500

    Create ingredients.py

---
+++ ingredients.py
@@ -0,0 +1,9 @@
+# Records information about ingredients
+
+ing_list = ['']
+
+ing = input('Enter the name of an ingredient')
+
+ing_cost = input('Enter the cost of this ingredient:')
+
+print(ing_cost,'is the cost of this ingredient.')
