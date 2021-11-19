1943957313e180f6927fe9beb3c66b4ae05489d4
bdilbeck <89172591+bdilbeck@users.noreply.github.com>
2021-11-18 20:49:55 -0500

    Update myplate.py

--- myplate.py
+++ myplate.py
@@ -1,23 +1,30 @@
-# Determine's a recipe's value on the Myplate system
+# Determines a recipe's value on the Myplate system
 def myplate():
     veg_yn = input('Does meal contain servings of vegitables?')
-    print(veg_yn)
     if veg_yn == 'yes':
         veg_s = input('How many?')
-        print(veg_s)
-    else:
-        print('No vegitable servings')
+    elif veg_yn == 'no':
+        veg_s = 0
     fruit_yn = input('Does meal contain servings of fruit?')
     if fruit_yn == 'yes':
         fruit_s = input('How Many?')
+    elif fruit_yn == 'no':
+        fruit_s = 0
     grains_yn = input('Does meal contain servings of grains?')
     if grains_yn == 'yes':
         grains_s = input('How Many?')
+    elif grains_yn == 'no':
+        grains_s = 0
     protien_yn = input('Does meal contain servings of protien?')
     if protien_yn == 'yes':
         protien_s = input('How Many?')
+    elif protien_yn == 'no':
+        protien_s = 0
 
+    print('This meal contains:')
+    print(veg_s,'serving(s) of vegitables')
+    print(fruit_s,'serving(s) of fruit')
+    print(grains_s,'serving(s) of grains')
+    print(protien_s,'serving(s) of protien')
 
 myplate()
-
-        
