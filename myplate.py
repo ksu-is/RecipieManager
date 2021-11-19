ddf152543d5a084cd177ef764566eb2c41a448d7
bdilbeck <89172591+bdilbeck@users.noreply.github.com>
2021-11-18 20:55:32 -0500

    Update myplate.py

--- myplate.py
+++ myplate.py
@@ -5,21 +5,29 @@ def myplate():
         veg_s = input('How many?')
     elif veg_yn == 'no':
         veg_s = 0
+    else:
+        print('invalid input!')
     fruit_yn = input('Does meal contain servings of fruit?')
     if fruit_yn == 'yes':
         fruit_s = input('How Many?')
     elif fruit_yn == 'no':
         fruit_s = 0
+    else:
+        print('invalid input!')
     grains_yn = input('Does meal contain servings of grains?')
     if grains_yn == 'yes':
         grains_s = input('How Many?')
     elif grains_yn == 'no':
         grains_s = 0
+    else:
+        print('invalid input!')
     protien_yn = input('Does meal contain servings of protien?')
     if protien_yn == 'yes':
         protien_s = input('How Many?')
     elif protien_yn == 'no':
         protien_s = 0
+    else:
+        print('invalid input!')
 
     print('This meal contains:')
     print(veg_s,'serving(s) of vegitables')
